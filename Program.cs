b46df7beda9fff9b02a52347758a03d71ba1b3fc
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-03 13:19:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace LoadingBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task task = new Task(() => Console.Write(i1));
+            //task.Start();
+
+            Console.Write("\n\n\n\n\n\n\n\n\n\n\n");
+            for (int i1 = 0; i1 < 45; i1++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Write(i1);
+                Thread.Sleep(150);
+                if (i1 == 44)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
